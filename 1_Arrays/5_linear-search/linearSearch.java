3276c440dd3ab8a5897b1356be35fe4948421524
Poojan Dave <poojandave0506@gmail.com>
2025-04-02 22:04:08 +0530

    lec 5 and 6

---
+++ 1_Arrays/5_linear-search/linearSearch.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class linearSearch {
+    public static int findLinearSearch(int numbers[], int key) {
+        for(int i = 0; i < numbers.length; i++) {
+            if(numbers[i] == key) {
+                return i;
+            }
+        }1
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = {1,2,3,4,5,6,7,8,9,10};
+        int key = 5;
+
+        int index = findLinearSearch(numbers, key);
+        if(index == -1) {
+            System.out.println("Not found");
+        } else {
+            System.out.println("Found at index: " + index);
+        }
+    }
+}
+
